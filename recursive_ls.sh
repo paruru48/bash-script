c0eefd2375e337ed9e5b2efb2ce2e9b16973ec38
paruru48 <thangnv48@gmail.com>
2016-06-22 11:14:49 +0700

    Update recursive_ls.sh

--- recursive_ls.sh
+++ recursive_ls.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#
+# Print out hadoop filesystem structure 
+
 thresh_depth="$2"
 
 function explore(){
@@ -6,7 +9,11 @@ function explore(){
     cur_depth="$2"
     for _child in $(hdfs dfs -ls "$path" 2>/dev/null | awk '{if(NF==8)print $8}'); do
         stat=$(hdfs dfs -stat "$_child" 2>/dev/null)
-        echo "$_child" "[$stat]"  | xargs -n 1 -I folder bash -c "f=\$(echo folder | awk '{print \$1}'); size=\$(hdfs dfs -du -s -h \$f 2>/dev/null | awk '{if(\$1!=0){print \$1\$2 } else print \"OM\"}' ); echo folder \$size" | sed -e 's/[^-][^\/]*\//--/g' -e 's/^/ /' -e 's/-/|/'
+        echo "$_child" "[$stat]"  \
+            | xargs -n 1 -I folder bash -c "f=\$(echo folder | awk '{print \$1}'); \
+                                            size=\$(hdfs dfs -du -s -h \$f 2>/dev/null | awk '{if(\$1!=0){print \$1\$2 } else print \"OM\"}' ); \
+                                            echo folder \$size" \
+            | sed -e 's/[^-][^\/]*\//--/g' -e 's/^/ /' -e 's/-/|/'
         if [[ "$cur_depth" -lt "$thresh_depth" ]]; then
             explore "$_child" $(( cur_depth + 1 ))
             let cur_depth=cur_depth-1
@@ -14,4 +21,4 @@ function explore(){
     done
 }
 
-explore "$1" 
+explore "$1" 1
